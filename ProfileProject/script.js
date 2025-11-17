7ee046a221f79a1f41d5fae28a38f293a058885c
FengCastelo <ianfcastelo7@gmail.com>
2025-11-17 19:04:45 -0300

    Fixing all the bugs and making this shit work.

--- ProfileProject/script.js
+++ ProfileProject/script.js
@@ -28,13 +28,14 @@ function loadProfile() {
     if (tema) document.getElementById("themeSelect").value = tema;
     if (cor) document.getElementById("fontColorSelect").value = cor;
     if (foto) document.getElementById("photoInput").value = foto;
+    console.log("Foto carregada:", foto);
 
     if (foto) {
-      document.getElementById("userPhoto").src = foto;
+      document.getElementById("userPhoto").src = "Pictures/"+foto+".png";
     } else {
       document.getElementById("userPhoto").src = "Pictures/default.png";
     }
-    mudartema();
+    changetheme();
     return;
   }
 
@@ -42,8 +43,6 @@ function loadProfile() {
 }
 
 function changetheme() {
-  const tema = document.getElementById("themeSelect").value;
-  const corFonte = document.getElementById("fontColorSelect").value;
   const body = document.body;
   const container = document.getElementById("profileContainer");
   const nameInput = document.getElementById("nameInput");
@@ -58,7 +57,7 @@ function changetheme() {
   container.className =
     "p-8 rounded-xl shadow-lg w-full max-w-sm flex flex-col gap-6";
   nameInput.className =
-    "w-full px-4 py-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500";
+    "w-full  px-4 py-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500";
   ageInput.className =
     "w-full px-4 py-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500";
   emailInput.className =
@@ -72,40 +71,17 @@ function changetheme() {
   signupBtn.className =
     "w-full bg-indigo-600 text-white px-4 py-3 rounded-lg hover:bg-indigo-700 transition-colors";
 
-  if (tema === "lightTheme") {
+  if (themeSelect.value === "lightTheme") {
     body.classList.add("bg-slate-100");
     container.classList.add("bg-slate-300");
-    nameInput.classList.add(
-      "bg-white",
-      "text-gray-900",
-      "placeholder-gray-500",
-      "border-gray-300"
-    );
-    ageInput.classList.add(
-      "bg-white",
-      "text-gray-900",
-      "placeholder-gray-500",
-      "border-gray-300"
-    );
-    emailInput.classList.add(
-      "bg-white",
-      "text-gray-900",
-      "placeholder-gray-500",
-      "border-gray-300"
-    );
+    nameInput.classList.add("bg-white","text-gray-900","placeholder-gray-500","border-gray-300");
+    ageInput.classList.add(  "bg-white",  "text-gray-900",  "placeholder-gray-500",  "border-gray-300");
+    emailInput.classList.add(  "bg-white",  "text-gray-900",  "placeholder-gray-500",  "border-gray-300");
     themeSelect.classList.add("bg-white", "text-gray-900", "border-gray-300");
-    fontColorSelect.classList.add(
-      "bg-white",
-      "text-gray-900",
-      "border-gray-300"
-    );
+    fontColorSelect.classList.add(  "bg-white",  "text-gray-900","border-gray-300");
     photoInput.classList.add("bg-white", "text-gray-900", "border-gray-300");
-    signupButton.classList.add(
-      "bg-white",
-      "text-gray-900",
-      "hover:bg-indigo-700"
-    );
-  } else if (tema === "nightTheme") {
+    signupBtn.classList.add(  "bg-white",  "text-gray-900",  "hover:bg-indigo-700");
+  } else if (themeSelect.value === "nightTheme") {
     body.classList.add("bg-gray-900");
     container.classList.add("bg-gray-800");
     nameInput.classList.add("bg-gray-900");
@@ -114,14 +90,10 @@ function changetheme() {
     themeSelect.classList.add("bg-gray-900");
     fontColorSelect.classList.add("bg-gray-900");
     photoInput.classList.add("bg-gray-900");
-    signupButton.classList.add(
-      "bg-gray-700",
-      "text-white",
-      "hover:bg-gray-600"
-    );
+    signupBtn.classList.add( "bg-gray-700", "text-white", "hover:bg-gray-600");
   }
 
-  if (corFonte === "darkFont") {
+  if (fontColorSelect.value === "darkFont") {
     body.classList.add("placeholder-gray-900");
     nameInput.classList.add("text-gray-900", "placeholder-gray-900");
     ageInput.classList.add("text-gray-900", "placeholder-gray-900");
@@ -129,19 +101,21 @@ function changetheme() {
     photoInput.classList.add("text-gray-900", "placeholder-gray-900");
     themeSelect.classList.add("text-gray-900", "placeholder-gray-900");
     fontColorSelect.classList.add("text-gray-900", "placeholder-gray-900");
-  } else if (corFonte === "lightFont") {
+  } else if (fontColorSelect.value === "lightFont") {
     body.classList.add("placeholder-gray-100");
-    nameInput.classList.add("text-gray-500", "placeholder-gray-500");
-    ageInput.classList.add("text-gray-500", "placeholder-gray-500");
-    emailInput.classList.add("text-gray-500", "placeholder-gray-500");
-    photoInput.classList.add("text-gray-500", "placeholder-gray-500");
-    themeSelect.classList.add("text-gray-500", "placeholder-gray-500");
-    fontColorSelect.classList.add("text-gray-500", "placeholder-gray-500");
+    nameInput.classList.add("text-gray-500");
+    ageInput.classList.add("text-gray-500");
+    emailInput.classList.add("text-gray-500");
+    photoInput.classList.add("text-gray-500");
+    themeSelect.classList.add("text-gray-500");
+    fontColorSelect.classList.add("text-gray-500" );
   }
+}
 
 function signUp() {
 
   const userId = localStorage.getItem("currentUserId");
+  console.log("User ID:", userId);
 
   if (!userId) {
     alert("Erro: Nenhum usuário logado!");
@@ -154,6 +128,13 @@ function signUp() {
   const tema = document.getElementById("themeSelect").value;
   const corFonte = document.getElementById("fontColorSelect").value;
   const foto = document.getElementById("photoInput").value.trim();
+
+  console.log("Nome:", nome);
+  console.log("Idade:", idade);
+  console.log("Email:", email);
+  console.log("Tema:", tema);
+  console.log("Cor da Fonte:", corFonte);
+  console.log("Foto:", foto);
 
   if (!nome || !idade || !email) {
     alert("Preencha Nome, Idade e Email.");
@@ -176,7 +157,6 @@ function signUp() {
   loadProfile();
 }
 
-}
 
 function logout() {
   localStorage.removeItem("currentUserId");
